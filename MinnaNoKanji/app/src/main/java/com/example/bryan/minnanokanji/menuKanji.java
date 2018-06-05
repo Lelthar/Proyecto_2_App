1f82b448d6f2b3da3c5e3bc2427168d2f8b67f14
Bryan Hernández Sibaja <bryan_2504@hotmail.com>
2018-06-05 04:30:35 -0600

    Act2

--- MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/menuKanji.java
+++ MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/menuKanji.java
@@ -2,6 +2,9 @@ package com.example.bryan.minnanokanji;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class menuKanji extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class menuKanji extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_kanji);
         getSupportActionBar().setTitle("Kanji 漢字");
+
+        //creación del botón de acordeón
+        Button findMagicBtn = (Button) findViewById(R.id.magic_btn);
+        findMagicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout findMagicLl = (LinearLayout) findViewById(R.id.magic_layout);
+                if (findMagicLl.getVisibility() == View.VISIBLE) {
+                    findMagicLl.setVisibility(View.GONE);
+                } else {
+                    findMagicLl.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
