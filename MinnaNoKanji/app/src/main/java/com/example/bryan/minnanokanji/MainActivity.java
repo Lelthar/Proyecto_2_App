7750bd87b04f4ae4514a0bb1a4928fe1b17d9ad9
lelthar <geraldm1998@hotmail.com>
2018-06-07 11:19:19 -0600

    Se agregó Mixpanel al proyecto

--- MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/MainActivity.java
+++ MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.bryan.minnanokanji;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Iniciar Sesión");
+
+        MixpanelAPI mixpanel = MixpanelAPI.getInstance(this,"4b4d6b196066ba9a609c13fb5a11360c");
+        mixpanel.track("Login",null);
+        mixpanel.flush();
     }
 }
