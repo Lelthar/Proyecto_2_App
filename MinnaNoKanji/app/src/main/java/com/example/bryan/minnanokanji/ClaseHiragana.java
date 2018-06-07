fbeefb8e9b93b9b7eec287740f9f6d86c81760e6
Bryan Hernández Sibaja <bryan_2504@hotmail.com>
2018-06-06 23:14:12 -0600

    Activities que faltaban
    Activities de Clases (Lecciones)
    Curiosidades de Japón

--- MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/ClaseHiragana.java
+++ MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/ClaseHiragana.java
@@ -3,12 +3,12 @@ package com.example.bryan.minnanokanji;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Curiosidades extends AppCompatActivity {
+public class ClaseHiragana extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_curiosidades);
-        getSupportActionBar().setTitle("Curiosidades de Japón");
+        setContentView(R.layout.activity_clase_hiragana);
+        getSupportActionBar().setTitle("HIRAGANA, Clase No. ");
     }
 }
