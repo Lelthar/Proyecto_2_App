a7ee66a7e4b8dde8e867764539af1633e3ec9ed1
reyesm2 <reyes.marlon57@gmail.com>
2018-06-07 20:33:51 -0600

    Problema_Compatibilidad

--- MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/MainActivity.java
+++ MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/MainActivity.java
@@ -2,10 +2,19 @@ package com.example.bryan.minnanokanji;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mixpanel.android.mpmetrics.MixpanelAPI;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText editTextEmail;
+    public EditText editTextPass;
+    public Button buttonInit;
+    public Button butttonRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Iniciar Sesión");
 
+        editTextEmail = findViewById(R.id.editTextMEmail);
+        editTextPass  = findViewById(R.id.editTextMPass);
+        buttonInit = findViewById(R.id.buttonMinit);
+        butttonRegister = findViewById(R.id.buttonMRegister);
+
         MixpanelAPI mixpanel = MixpanelAPI.getInstance(this,"4b4d6b196066ba9a609c13fb5a11360c");
         mixpanel.track("Login",null);
         mixpanel.flush();
+
+        butttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
 }
