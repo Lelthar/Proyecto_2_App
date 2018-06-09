c27810ae0c539e11c3c956531387ea3d99cd5cb2
Bryan Hernández Sibaja <bryan_2504@hotmail.com>
2018-06-09 02:14:28 -0600

    link activities

--- MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/menuJapon.java
+++ MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/menuJapon.java
@@ -1,7 +1,10 @@
 package com.example.bryan.minnanokanji;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menuJapon extends AppCompatActivity {
 
@@ -11,5 +14,32 @@ public class menuJapon extends AppCompatActivity {
         setContentView(R.layout.activity_menu_japon);
         getSupportActionBar().setTitle("Acerca de Japón");
 
+        Button btnCuriosidades = (Button) findViewById(R.id.btnCuriosidad);
+        Button btnConvertidor= (Button) findViewById(R.id.btnConvertidor);
+        Button btnEmbajada= (Button) findViewById(R.id.btnEmbajada);
+
+        btnCuriosidades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(menuJapon.this, Curiosidades.class);
+                startActivity(intent);
+            }
+        });
+
+        btnConvertidor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(menuJapon.this, Convertidor.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEmbajada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(menuJapon.this, Embajada.class);
+                startActivity(intent);
+            }
+        });
     }
 }
