0f0a34e3b7b92107a15840fea59e2eeb11e37958
Bryan Hernández Sibaja <bryan_2504@hotmail.com>
2018-06-12 00:24:32 -0600

    cambios curiosidades

--- MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/Curiosidades.java
+++ MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/Curiosidades.java
@@ -2,6 +2,8 @@ package com.example.bryan.minnanokanji;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Curiosidades extends AppCompatActivity {
 
@@ -10,5 +12,41 @@ public class Curiosidades extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curiosidades);
         getSupportActionBar().setTitle("Curiosidades de Japón");
+
+
+        Button btnFiltro = (Button) findViewById(R.id.btnFiltrar);
+        btnFiltro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* ACÁ SE HACE LA FILTRACIÓN */
+            }
+        });
+
+
+        Button btnOrden = (Button) findViewById(R.id.btnOrdenar);
+        btnOrden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* ACA SE HACE EL ORDENAMIENTO */
+            }
+        });
+
+
+        final Button UpDown = (Button) findViewById(R.id.UpDown);
+        UpDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (UpDown.getText().toString()=="↑"){
+                    UpDown.setText("↓");
+                }
+                else{
+                    UpDown.setText("↑");
+                }
+            }
+        });
+
+
+
+
     }
 }
