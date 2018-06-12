39beb2d8b0830dc25f581c100c765e9eead43430
reyesm2 <reyes.marlon57@gmail.com>
2018-06-12 06:19:26 -0600

    Ultimo_merge_sin_m

--- MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/menuKanji.java
+++ MinnaNoKanji/app/src/main/java/com/example/bryan/minnanokanji/menuKanji.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;m
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
